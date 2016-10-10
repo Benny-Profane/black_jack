a748eac30e6f4a27599136fbf875d86f151fb23b
James Costello <jcostello10@gmail.com>
2016-10-09 22:53:27 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+//Model
+
+// var house = []
+// var player = []
+// var deck = [1,2,3....52]
+// var playerBetTotal = Starts at $200  //
+
+
+// var houseScore = house must [17-21]
+// var playerScore = [0-21]
+
+//Funcitons
+
+// var deal = funciton() { deck[random[0,1]] to player[] + deck[random[2,3]] to house[] }
+// var hit = funciton () { deck[random[4...5...etc]] to player }
+// var stay = stay button triggers winfunction
+// var playerwin = function() { compare house[] to player[] } if (playerScore > houseScore && playerScore is < 22) { "YOU WIN!"}
+// var gameOver = if (playerTotal === 0) { alert("YOU'RE OUTTA LUCK BUDDY!")}
+
+// Shuffle function
+// function shuffle(array) {
+//   var m = array.length, t, i;
+//   while (m) {
+//     i = Math.floor(Math.random() * m--);
+//     t = array[m];
+//     array[m] = array[i];
+//     array[i] = t;
+//   }
+// }
+
+$('#deal').on('click', function {
+
+})
+
